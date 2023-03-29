0fa1de124bdc901b2c61530e9b0ec62524804537
Rafał Nowak <rwojciechnowak@gmail.com>
2023-03-29 10:50:31 +0200

    first commit

---
+++ src/main/java/GCGroup/GameChanger/GameChangerApplication.java
@@ -0,0 +1,13 @@
+package GCGroup.GameChanger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameChangerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameChangerApplication.class, args);
+	}
+
+}
