c4d513352ff55ea4cce4a184f389abc87f4c2c12
Rafał Nowak <rwojciechnowak@gmail.com>
2023-06-02 15:38:06 +0200

    feat: folder restructuring, basic cart & data

---
+++ src/main/java/GCGroup/GameChanger/Controllers/ProfileController.java
@@ -0,0 +1,12 @@
+package GCGroup.GameChanger.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ProfileController {
+    @GetMapping("/profile")
+    public String showProfileForm() {
+      return "profile";
+    }
+}
