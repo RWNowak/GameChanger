ec45fe8b550ef49ecedbb37366e2c0349d74c7b8
jgabryszewski01 <jgabryszewski01@gmail.com>
2023-06-04 13:26:20 +0200

    fix: registration fixed

--- src/main/java/GCGroup/GameChanger/Controllers/LoginController.java
+++ src/main/java/GCGroup/GameChanger/Controllers/LoginController.java
@@ -1,15 +1,46 @@
 package GCGroup.GameChanger.Controllers;
 
+import GCGroup.GameChanger.Data.User;
+import GCGroup.GameChanger.Data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 
-  @GetMapping("/login")
-  public String showLoginForm() {
-    return "login";
-  }
+  private final UserRepository userRepository;
+
+    @Autowired
+    public LoginController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String loginUser(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            RedirectAttributes redirectAttributes) {
+
+        User user = userRepository.findByUsername(username);
+
+        if (user == null || !user.getPassword().equals(password)) {
+            redirectAttributes.addFlashAttribute("error", "Incorrect username or password.");
+            return "redirect:/login";
+        }
+
+
+
+        return "redirect:/dashboard";
+    }
 
 }
 
--- src/main/java/GCGroup/GameChanger/Controllers/RegisterController.java
+++ src/main/java/GCGroup/GameChanger/Controllers/RegisterController.java
@@ -4,6 +4,7 @@ import GCGroup.GameChanger.Data.User;
 import GCGroup.GameChanger.Data.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -33,12 +34,12 @@ public class RegisterController {
             RedirectAttributes redirectAttributes) {
 
         if (!password.equals(password2)) {
-            redirectAttributes.addFlashAttribute("error", "Powtórzone hasło musi być identyczne.");
+            redirectAttributes.addFlashAttribute("error", "Passwords do not match.");
             return "redirect:/register";
         }
 
         if (userRepository.findByUsername(username) != null) {
-            redirectAttributes.addFlashAttribute("error", "Użytkownik o podanej nazwie już istnieje.");
+            redirectAttributes.addFlashAttribute("error", "This username already exists.");
             return "redirect:/register";
         }
 
@@ -49,7 +50,7 @@ public class RegisterController {
 
         userRepository.save(user);
 
-        redirectAttributes.addFlashAttribute("success", "Rejestracja zakończona pomyślnie. Możesz się zalogować.");
+        redirectAttributes.addFlashAttribute("success", "registration successful. Now you are able to log in.");
 
         return "redirect:/login";
     }
